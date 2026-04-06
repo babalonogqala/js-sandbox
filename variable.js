5afcfb640f5aa58abc8791ca9f4c899a4476b7f9
babalonogqala <babalonogqala2003@gmail.com>
2026-04-06 11:44:24 +0200

    character length

--- variable.js
+++ variable.js
@@ -1,9 +1,20 @@
 var a = 3;
 var b = 8;
 
-var c;
-c = a;
+var c = a;
+
 a = b;
 b = c;
 console.log(a);
 console.log(b);
+
+var output = prompt("character num");
+let num = output.length;
+var left = 600 - num;
+
+// var char = function lenghtcalc() {
+//   let num = output.length;
+//   num - 200;
+// };
+
+console.log(num + " plus only left " + left);
