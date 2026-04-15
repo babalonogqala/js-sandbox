8d3eab2c56e057489a4cae308799d59236487240
babalonogqala <babalonogqala2003@gmail.com>
2026-04-15 09:10:04 +0200

    js functions

--- variable.js
+++ variable.js
@@ -8,13 +8,38 @@ b = c;
 console.log(a);
 console.log(b);
 
-var output = prompt("character num");
-let num = output.length;
-var left = 600 - num;
+// var output = prompt("character num");
+// let num = output.length;
+// var left = 600 - num;
 
-// var char = function lenghtcalc() {
-//   let num = output.length;
-//   num - 200;
-// };
+// console.log(num + " plus only left " + left);
 
-console.log(num + " plus only left " + left);
+//slice funtions
+// .slice(0, 10);
+
+let name = "babalo";
+console.log(name.toUpperCase());
+
+var prompt = prompt("enter name");
+
+alert(prompt.toUpperCase());
+function getMilk() {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
